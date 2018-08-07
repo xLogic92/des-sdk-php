b3d577afc94e71fd0625ce6cabdcf106aa89b8ef
shinan <932612011@qq.com>
2018-08-07 17:57:00 +0800

    common  http

--- examples/DESMerchantClientTest.php
+++ examples/DESMerchantClientTest.php
@@ -1,15 +1,15 @@
 <?php
 require __DIR__ . "/../vendor/autoload.php";
 
-use GXChain\Client\DESMerchantClient;
+use GXChain\DES\DESMerchantClient;
 
-$privateKey = '5Ka9YjFQtfUUX2Ddnqka...'; // Private Key})
-$accountId = '1.2.19'; // Account ID
+$privateKey        = '5Ka9YjFQtfUUX2Ddnqka...'; // Private Key})
+$accountId         = '1.2.19'; // Account ID
 $DESMerchantClient = new DESMerchantClient($privateKey, $accountId);
 
-$testCase = (object)array(
-	'name' => 'XXX',
-    'idcard' => 'XXXXXXXXXXXXXXXXXX'
+$testCase = (object) array(
+    'name'   => 'XXX',
+    'idcard' => 'XXXXXXXXXXXXXXXXXX',
 );
 
 $DESMerchantClient->createDataExchangeRequest($testCase, 2, function ($res) use ($DESMerchantClient) {
